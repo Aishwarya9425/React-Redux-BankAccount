6fdb02d31aba0ac4947e2c6f76a1dbf68446071d
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-17 18:16:11 +0530

    refactoring with Redux toolkit - configureStore

--- src/store.js
+++ src/store.js
@@ -1,19 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-// 'createStore' is deprecated
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-/* thunk -- middleware, b/w dispatcher and store, used for side effects before reaching the reducer and updating the store; */
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
---
+++ src/store_createStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+// 'createStore' is deprecated
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+/* thunk -- middleware, b/w dispatcher and store, used for side effects before reaching the reducer and updating the store; */
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
